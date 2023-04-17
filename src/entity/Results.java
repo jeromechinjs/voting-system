b3de7b34eec6530595f99e698de05ee59f9754ec
bryanwtarc <bryanwbl-wp20@student.tarc.edu.my>
2023-04-17 20:11:54 +0800

    +Results.java

--- src/entity/Results.java
+++ src/entity/Results.java
@@ -2,13 +2,14 @@ package entity;
 import adt.*;
 
 public class Results {
-    private 
-    public Results(){
-
-    }
+    public Results(){}
 
     public void printResults() {
-        System.out.println("Thank you for your paticipation in the voting of Teacher of The Year!");
+        System.out.printf("Thank you for your paticipation in the voting of Teacher of The Year!\n" +
+                           "Below are the results for The Teacher of The Year: ");
+
+        System.out.printf 
+
     }
    
 }
