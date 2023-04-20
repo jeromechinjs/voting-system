167f1e5bb0c52323de26a3a5b20351fae64bd16a
bryanwtarc <bryanwbl-wp20@student.tarc.edu.my>
2023-04-20 20:42:28 +0800

    commit

--- src/entity/Results.java
+++ src/entity/Results.java
@@ -1,16 +1,22 @@
 package entity;
 import adt.*;
-
+/**
+ *
+ * @author Bryan Wong
+ */
 public class Results {
     private ArrayStack<Integer> teachers;
-
+    
+    
     public Results(){}
 
     public void printResults() {
         System.out.printf("Thank you for your paticipation in the voting of Teacher of The Year!\n" +
                            "Below are the results for The Teacher of The Year: ");
 
-        System.out.printf("1. " + teachers.pop());
+        for (int i = 0; i < teachers.getNumberOfEntries(); i++) {
+            System.out.printf(i + ". " + teachers.pop() + "\n");
+        }
 
     }
    
