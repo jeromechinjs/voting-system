6cf1c5501bbc687c4c94a9e20a0e1e8f5e352cfe
YenKhang Chong <chongyk-wp@student.tarc.edu.my>
2023-04-21 23:08:40 +0800

    Finished maintenance on admin

--- src/client/Main.java
+++ src/client/Main.java
@@ -37,7 +37,6 @@ public class Main {
                 break;
             }
         }
-        int temp = 0;
         switch (selection) {
             case "1":
                 linkedAdminList.displayAdminList();
@@ -58,22 +57,35 @@ public class Main {
                     System.out.println("*\t[4]\tAdd Admin\t\t\t*");
                     System.out.println("*\t[5]\tExit\t\t\t*");
                     System.out.println("\n*********************************************************\n\n");
-                    int result;
-                    while (true) {
-                        try {
-                            System.out.print("Selection (1/2/3/4/5): ");
-                            selection = sc.next();
-                            result = Integer.parseInt(selection);
-                        } catch (Exception e) {
-                            System.out.println("Invalid input, please try again.");
-                            continue;
-                        }
-                        if (result < 1 | result > 5) {
-                            System.out.print("\nInvalid input! Please select the numbers available. ");
-                        } else {
+                    int sel;
+                    System.out.print("Choose 1 - 5: ");
+                    sel = sc.nextInt();
+                    switch (sel) {
+                        case 1:
+                            String cName;
+                            System.out.print("\nEnter Candidate Name: ");
+                            cName = sc.next();
+                            linkedCandidateList.addCandidate(cName);
                             break;
-                        }
-                        temp = result;
+                        case 2:
+                            String cand;
+                            System.out.print("\nEnter Candidate Name: ");
+                            cand = sc.next();
+                            linkedCandidateList.removeCandidate(cand);
+                            break;
+                        case 3:
+                            linkedCandidateList.displayCandidateList();
+                            break;
+                        case 4:
+                            String aName, aPwd;
+                            System.out.print("\nEnter admin userame: ");
+                            aName = sc.next();
+                            System.out.print("\nEnter password for account: ");
+                            aPwd = sc.next();
+                            linkedAdminList.addAdmin(aName, aPwd);
+                            break;
+                        case 5:
+                            break;
                     }
                 }
                 break;
@@ -83,51 +95,6 @@ public class Main {
             case "3":
                 System.out.println("Have a good day!");
                 break;
-        }
-
-        switch (temp) {
-            case 1:
-                String repeat, checkSelect;
-                int validate = 1;
-                do {
-                    String cName;
-                    System.out.print("\nEnter Candidate Name: ");
-                    cName = sc.next();
-                    linkedCandidateList.addCandidate(cName);
-                    System.out.println("Do you want to add more? 1 for yes 0 for no: ");
-                    while (true) {
-                        try {
-                            System.out.print("Selection (0/1): ");
-                            repeat = sc.next();
-                            validate = Integer.parseInt(repeat);
-                        } catch (Exception e) {
-                            System.out.println("Invalid input, please try again.");
-                            continue;
-                        }
-                        if (validate < 0 | validate > 1) {
-                            System.out.print("\nInvalid input! Please select the numbers available. ");
-                        } else {
-                            break;
-                        }
-                    }
-                } while (validate == 1);
-                break;
-            case 2:
-                String cand;
-                System.out.print("\nEnter Candidate Name: ");
-                cand = sc.next();
-                linkedCandidateList.removeCandidate(cand);
-                break;
-            case 3:
-                linkedCandidateList.displayCandidateList();
-                break;
-            case 4:
-                String aName, aPwd;
-                System.out.print("\nEnter admin userame: ");
-                aName = sc.next();
-                System.out.print("\nEnter password for account: ");
-                aPwd = sc.next();
-                linkedAdminList.addAdmin(aName, aPwd);
         }
     }
 }
