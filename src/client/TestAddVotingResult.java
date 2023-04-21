98a971471f1d7bcc06f9f8fe294ef01a4797b89b
ions-js04 <behnc.natural@gmail.com>
2023-04-21 20:24:51 +0800

    msg

--- src/client/TestAddVotingResult.java
+++ src/client/TestAddVotingResult.java
@@ -1,14 +1,11 @@
 package client;
 
-import java.util.Iterator;
 import adt.ArrayQueue;
 import adt.QueueInterface;
 import entity.VoteResultDataPair;
 
-// temporary list (for teacher names)
-import adt.ArrayList;
-
 import java.util.Scanner;
+import java.util.Iterator;
 
 public class TestAddVotingResult {
     public static void main(String args[]) {
@@ -28,60 +25,14 @@ public class TestAddVotingResult {
         voteResults.enqueue(new VoteResultDataPair("candidateI", 0));
         voteResults.enqueue(new VoteResultDataPair("candidateJ", 0));
 
-
-        boolean cont = true;
-
-         // get iterator
-         Iterator<VoteResultDataPair> iterator = voteResults.iterator();
+    // get iterator
+    Iterator<VoteResultDataPair> iterator = voteResults.getIterator();
         
-         while (iterator.hasNext()) { // traverse through each dataPair in voteResults arrayQueue
-             voteResults = iterator.next();
-             
- 
-             }
-             
-         }
-            
+    while (iterator.hasNext()) { // traverse through each dataPair in voteResults arrayQueue
+        voteResults = iterator.next();
         
-        do {
-             
-            String registerId = sc.nextLine();
-        
-        
-            System.out.print("Enter student name: ");
-            String registerName = sc.nextLine();
-        
-            System.out.print("Enter student faculty: ");
-            String faculty = sc.nextLine();
-        
-            System.out.print("Enter password: ");
-            String registerPassword = sc.nextLine();
-        
-            Voter voter = new Voter(registerId, registerName, faculty, registerPassword);
-        
-            voterList.add(voter);
-            
-            System.out.print("Continue?: ");
-            String yes = sc.nextLine();
-            cont = "y".equals(yes);
-            
-        } while (cont);
-       
-        // displaying info in array list
-//        for (int i = 1; i <= voterList.getNumberOfEntries(); i++) {
-//            System.out.print("ID: " + voterList.getEntry(i).getId() + "\nName: " + voterList.getEntry(i).getName() + "\nFaculty: " + voterList.getEntry(i).getFaculty());
-//            
-//        }
-        
-        boolean conti = true;
-        do {
-            System.out.println("Student Login");
-        System.out.print("Enter student id: ");
-            String id = sc.nextLine();
-
-            System.out.print("Enter password: ");
-            String password = sc.nextLine();
-            
-      
     }
+            
+        
+        
 }
