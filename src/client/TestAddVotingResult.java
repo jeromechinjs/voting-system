25ad9e62a70dc188052eb0e33fa4afd156aa4f69
ions-js04 <behnc.natural@gmail.com>
2023-04-21 22:23:39 +0800

    asdsdf

--- src/client/TestAddVotingResult.java
+++ src/client/TestAddVotingResult.java
@@ -1,8 +1,6 @@
 package client;
-
-import adt.ArrayQueue;
-import adt.QueueInterface;
-import entity.VoteResultDataPair;
+import adt.*;
+import entity.*;
 
 import java.util.Scanner;
 import java.util.Iterator;
@@ -10,28 +8,29 @@ import java.util.Iterator;
 public class TestAddVotingResult {
     public static void main(String args[]) {
         
-        ArrayQueue<VoteResultDataPair> voteResults = new ArrayQueue<VoteResultDataPair>();
+        QueueInterface<VoteResultDataPair> voteResults = new ArrayQueue<VoteResultDataPair>();
         
         Scanner sc = new Scanner(System.in); 
 
         // temporary
         voteResults.enqueue(new VoteResultDataPair("candidateA", 0));
-        voteResults.enqueue(new VoteResultDataPair("candidateB", 0));
+        voteResults.enqueue(new VoteResultDataPair("candidateB", 5));
         voteResults.enqueue(new VoteResultDataPair("candidateC", 0));
         voteResults.enqueue(new VoteResultDataPair("candidateE", 0));
         voteResults.enqueue(new VoteResultDataPair("candidateF", 0));
-        voteResults.enqueue(new VoteResultDataPair("candidateG", 0));
+        voteResults.enqueue(new VoteResultDataPair("candidateG", 5));
         voteResults.enqueue(new VoteResultDataPair("candidateH", 0));
-        voteResults.enqueue(new VoteResultDataPair("candidateI", 0));
+        voteResults.enqueue(new VoteResultDataPair("candidateI", 3));
         voteResults.enqueue(new VoteResultDataPair("candidateJ", 0));
 
-    // get iterator
+    //get iterator
     Iterator<VoteResultDataPair> iterator = voteResults.getIterator();
-        
-    while (iterator.hasNext()) { // traverse through each dataPair in voteResults arrayQueue
-        voteResults = iterator.next();
-        
+    VoteResultDataPair a;
+    while (iterator.hasNext()) { // check if arrayQueue has any more elements - traverse through each dataPair in voteResults arrayQueue
+        a = iterator.next();
+        System.out.print(voteResults.getFront().getVoteCount());
     }
+}
             
         
         
