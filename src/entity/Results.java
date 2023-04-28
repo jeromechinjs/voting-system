ffa4e1fa5838db02a55cc55bef33798f2727bac5
bryanwtarc <bryanwbl-wp20@student.tarc.edu.my>
2023-04-28 18:57:27 +0800

    done Results

--- src/entity/Results.java
+++ src/entity/Results.java
@@ -26,13 +26,16 @@ public class Results {
             voteStack.push(result.getVoteCount());
         }
         
-        sortedStack.sortAscending(voteStack, nameStack);
+        sortedStack = sortedStack.sortAscending(voteStack, nameStack);
+        voteStack.clear();
+        nameStack.clear();
 
         System.out.printf("Thank you for your paticipation in the voting of Teacher of The Year!\n" +
                            "Below are the results for The Teacher of The Year:\n");
         System.out.printf("\n%-4s %-15s %-15s\n", "No.", "Teacher", "Vote Count");
         for (int i = 0; i < (sortedStack.getNumberOfEntries()); i++) {
-            System.out.printf("%-4d %-15s %-2d\n", i+1, sortedStack.pop().getCandidateName());
+            System.out.printf("%-4d %-15s %-2d\n", i+1, sortedStack.peek().getCandidateName(), sortedStack.peek().getVoteCount());
+            sortedStack.pop();
         }
 
     }
